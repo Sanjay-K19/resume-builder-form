d5b80c38a67f388152b4e20de72e662246e41cd9
Sanjay Kushwaha <mrskkushwaha143@gmail.com>
2025-07-31 19:09:26 +0545

    This const variables stores value from form by help of document.getElementById().value

--- form/script.js
+++ form/script.js
@@ -1,3 +1,15 @@
 document.getElementById('resume-form').addEventListener('submit', function(event) {
   event.preventDefault();   // prevent actual form submission
+
+   //Collect data of the form
+  const fullName = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const phone = document.getElementById('phone').value;
+  const jobTitle = document.getElementById('jobTitle').value;
+  const address = document.getElementById('address').value;
+  const education = document.getElementById('education').value;
+  const skills = document.getElementById('skills').value;
+  const summary = document.getElementById('summary').value;
+  const experience = document.getElementById('experience').value;
+  const reference = document.getElementById('reference').value;
 })
